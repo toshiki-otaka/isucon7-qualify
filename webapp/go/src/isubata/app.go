e473c159039b41ab4b9967b4c8ede886e07449cc
大高俊輝 <toshiki.otaka@candee.co.jp>
2021-08-15 19:05:27 +0900

    revert

--- webapp/go/src/isubata/app.go
+++ webapp/go/src/isubata/app.go
@@ -122,28 +122,6 @@ type Message struct {
 	CreatedAt time.Time `db:"created_at"`
 }
 
-type Messages []*Message
-
-func (ms Messages) CountByIDAndChannelID(id, channelID int64) int64 {
-	var count int64
-	for _, m := range ms {
-		if m.ChannelID == channelID && id < m.ID {
-			count++
-		}
-	}
-	return count
-}
-
-func (ms Messages) CountByChannelID(channelID int64) int64 {
-	var count int64
-	for _, m := range ms {
-		if m.ChannelID == channelID {
-			count++
-		}
-	}
-	return count
-}
-
 func queryMessages(chanID, lastID int64) ([]Message, error) {
 	msgs := []Message{}
 	err := db.Select(&msgs, "SELECT * FROM message WHERE id > ? AND channel_id = ? ORDER BY id DESC LIMIT 100",
@@ -437,28 +415,14 @@ func queryChannels() ([]int64, error) {
 	return res, err
 }
 
-type HaveRead struct {
-	UserID    int64     `db:"user_id"`
-	ChannelID int64     `db:"channel_id"`
-	MessageID int64     `db:"message_id"`
-	UpdatedAt time.Time `db:"updated_at"`
-	CreatedAt time.Time `db:"created_at"`
-}
-
-type HaveReads []*HaveRead
-
-func (ms HaveReads) LastMessageID(userID, channelID int64) int64 {
-	var lastMessageID int64
-	for _, m := range ms {
-		if m.UserID == userID && m.ChannelID == channelID {
-			lastMessageID = m.MessageID
-			break
-		}
-	}
-	return lastMessageID
-}
-
 func queryHaveRead(userID, chID int64) (int64, error) {
+	type HaveRead struct {
+		UserID    int64     `db:"user_id"`
+		ChannelID int64     `db:"channel_id"`
+		MessageID int64     `db:"message_id"`
+		UpdatedAt time.Time `db:"updated_at"`
+		CreatedAt time.Time `db:"created_at"`
+	}
 	h := HaveRead{}
 
 	err := db.Get(&h, "SELECT * FROM haveread WHERE user_id = ? AND channel_id = ?",
@@ -478,35 +442,38 @@ func fetchUnread(c echo.Context) error {
 		return c.NoContent(http.StatusForbidden)
 	}
 
-	channelIDs, err := queryChannels()
+	time.Sleep(time.Second)
+
+	channels, err := queryChannels()
 	if err != nil {
 		return err
 	}
 
-	haveReads := HaveReads{}
-	if err = db.Select(&haveReads, "SELECT * FROM haveread WHERE user_id = ?", userID); err != sql.ErrNoRows && err != nil {
-		return err
-	}
+	resp := []map[string]interface{}{}
 
-	sqlQuery, params, err := sqlx.In("SELECT * FROM message WHERE channel_id in (?)", channelIDs)
-	if err != nil {
-		return err
-	}
-	messages := Messages{}
-	if err := db.Select(&messages, sqlQuery, params...); err != sql.ErrNoRows && err != nil {
-		return err
-	}
+	for _, chID := range channels {
+		lastID, err := queryHaveRead(userID, chID)
+		if err != nil {
+			return err
+		}
 
-	resp := make([]map[string]int64, len(channelIDs))
-	for i, chID := range channelIDs {
-		lastID := haveReads.LastMessageID(userID, chID)
 		var cnt int64
 		if lastID > 0 {
-			cnt = messages.CountByIDAndChannelID(lastID, chID)
+			err = db.Get(&cnt,
+				"SELECT COUNT(*) as cnt FROM message WHERE channel_id = ? AND ? < id",
+				chID, lastID)
 		} else {
-			cnt = messages.CountByChannelID(chID)
+			err = db.Get(&cnt,
+				"SELECT COUNT(*) as cnt FROM message WHERE channel_id = ?",
+				chID)
 		}
-		resp[i] = map[string]int64{"channel_id": chID, "unread": cnt}
+		if err != nil {
+			return err
+		}
+		r := map[string]interface{}{
+			"channel_id": chID,
+			"unread":     cnt}
+		resp = append(resp, r)
 	}
 
 	return c.JSON(http.StatusOK, resp)
